3592ec7afe7f5548bafa54424f6ad7970aafb94d
Nathalia-Urenha <naty.urenha@gmail.com>
2022-06-22 15:03:34 -0300

    Organização

--- 6 - Redux com React/react-app/src/App.js
+++ 6 - Redux com React/react-app/src/App.js
@@ -1,13 +1,14 @@
-import React from 'react';
-import { useDispatch, useSelector } from 'react-redux';
+import { useDispatch, useSelector } from "react-redux";
+import {incrementar, reduzir} from './store/contador';
 
 function App() {
-  const state = useSelector((state) => state);
+  const state = useSelector(state => state);
   const dispatch = useDispatch();
   return (
     <div>
       <h1>Total: {state}</h1>
-      <button onClick={() => dispatch({type: 'INCREMENTAR'})}>Incrementar</button>
+      <button onClick={() => dispatch(incrementar())}>Incrementar</button>
+      <button onClick={() => dispatch(reduzir())}>Reduzir</button>
     </div>
   )
 
--- 6 - Redux com React/react-app/src/index.js
+++ 6 - Redux com React/react-app/src/index.js
@@ -2,18 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import { Provider } from 'react-redux';
-import { createStore } from 'redux';
+import store from './store/configureStore';
 
-function reducer(state = 0, action) {
-  switch (action.type) {
-    case 'INCREMENTAR':
-      return state + 1;
-    default:
-      return state;
-  }
-}
-
-const store = createStore(reducer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
