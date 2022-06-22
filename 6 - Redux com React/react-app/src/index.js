1fed5977bf30bb48608978b18f63b30d1ca2db86
Nathalia-Urenha <naty.urenha@gmail.com>
2022-06-22 14:13:45 -0300

    Redux com react

---
+++ 6 - Redux com React/react-app/src/index.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+
+function reducer(state = 0, action) {
+  switch (action.type) {
+    case 'INCREMENTAR':
+      return state + 1;
+    default:
+      return state;
+  }
+}
+
+const store = createStore(reducer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </React.StrictMode>
+);
+
